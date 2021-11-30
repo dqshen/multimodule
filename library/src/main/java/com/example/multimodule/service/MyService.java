bbb2c9ba4db49904cb25fa87b36ae5df1d4b401e
dqs <dqs@dqs.com>
2021-11-30 13:13:05 +0800

    init

---
+++ library/src/main/java/com/example/multimodule/service/MyService.java
@@ -0,0 +1,10 @@
+package com.example.multimodule.service;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableConfigurationProperties(ServiceProperties.class)
+public class MyService {
+    
+}
